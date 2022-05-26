a02e534596520c81ada9add96c0971b8ced235b5
davur <davur@davur.net>
2022-05-26 22:06:23 +1000

    Add automatic admin registration of all models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,28 @@
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
+# from book.models import Book
+# 
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'author')
+# 
+# admin.site.register(Book, BookAdmin)
+
+
+
+# Automatic registration of all
+# Credit: https://hackernoon.com/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
+
+class ListAdminMixin(object):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+models = apps.get_models()
+for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+    try:
+        admin.site.register(model, admin_class)
+    except admin.sites.AlreadyRegistered:
+        pass
+
